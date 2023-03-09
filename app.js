7207623eba50d34acbc1eb181d5399e26412c18b
Sagarika Sil <sagarikasil07@gmail.com>
2023-03-09 20:54:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+(function($) { "use strict";
+	$('body').on('mouseenter mouseleave','.nav-item',function(e){
+			if ($(window).width() > 750) {
+				var _d=$(e.target).closest('.nav-item');_d.addClass('show');
+				setTimeout(function(){
+				_d[_d.is(':hover')?'addClass':'removeClass']('show');
+				},1);
+			}
+	});	
+
+  })(jQuery); 
