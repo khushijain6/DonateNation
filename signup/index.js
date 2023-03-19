0ebea406eb11c1ccb6a1a4dc4d0cb25003003909
Khushi Jain <jain.khushi2050@gmail.com>
2023-03-19 09:23:02 +0530

    events tab removed

--- signup/index.js
+++ signup/index.js
@@ -9,7 +9,6 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
     extended: true
 }))
-
 mongoose.connect('mongodb://0.0.0.0:27017/mydb', {
     useNewUrlParser: true,
     useUnifiedTopology: true
